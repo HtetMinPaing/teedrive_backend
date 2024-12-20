b6209dc81337db93061fbe942c056abbca34c729
Your Name~ <hmphtetminpaing@gmail.com>
2024-12-20 09:29:29 +0630

    first commit

---
+++ src/main/java/com/example/teedrive/TeedriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.teedrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeedriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeedriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teedrive/TeedriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teedrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeedriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
