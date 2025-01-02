0a75d30e75dd01044c683c60aeef3a0a8e24c45d
Your Name~ <hmphtetminpaing@gmail.com>
2025-01-02 10:15:19 +0630

    UserDetailService

---
+++ src/main/java/com/example/teedrive/domain/entity/RoleEntity.java
@@ -0,0 +1,19 @@
+package com.example.teedrive.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+}
--- src/main/java/com/example/teedrive/domain/entity/UserEntity.java
+++ src/main/java/com/example/teedrive/domain/entity/UserEntity.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Data
@@ -30,6 +32,14 @@ public class UserEntity {
     private String password;
 
     private String avatar;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private List<RoleEntity> roles = new ArrayList<>();
 
 //    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 //    private Set<FileEntity> ownedFiles;
